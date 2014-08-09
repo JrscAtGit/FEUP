a2ae22d6c244a37a1cf70902d96e988310d0fd3b
JRSC <ei12161@fe.up.pt>
2014-08-09 10:55:00 +0100

    first

---
+++ Other/DXF/DXF/_main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DXF
+//
+//  Created by José Ricardo de Sousa Coutinho on 09/08/14.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
